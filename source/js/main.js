2d1bdb325bb5aa54c512794ab0e58cc5db9947c8
Trouble-Andrew <satyr663@mail.ru>
2021-06-28 18:43:38 +0300

    Добавляет основные компоненты

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
-import {ieFix} from './utils/ie-fix';
-import {iosVhFix} from './utils/ios-vh-fix';
+import { ieFix } from './utils/ie-fix';
+import { iosVhFix } from './utils/ios-vh-fix';
 
-import {initModals} from './modules/init-modals';
+import { initModals } from './modules/init-modals';
 
 // Utils
 // ---------------------------------
@@ -13,3 +13,23 @@ iosVhFix();
 // ---------------------------------
 
 initModals();
+
+const firstScreen = document.querySelector('.first-screen');
+const mainButton = document.querySelector('.first-screen__button');
+const leftScreen = document.querySelector('.left-screen');
+const rightScreen = document.querySelector('.right-screen');
+
+mainButton.addEventListener('click', () => {
+  firstScreen.style.opacity = '0';
+  setTimeout(() => {
+    firstScreen.classList.add('visually-hidden');
+  }, 500);
+});
+
+leftScreen.addEventListener('click', () => {
+  rightScreen.classList.add('right-screen--visible');
+});
+
+rightScreen.addEventListener('click', () => {
+  rightScreen.classList.remove('right-screen--visible');
+});
